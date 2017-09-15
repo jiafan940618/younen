33274ab120e89cb000b1071ef924a473bf75802d
cmy <1097842122@qq.com>
2017-09-15 17:37:38 +0800

    并网发电评分

--- yn-core/src/main/java/com/yn/service/ApolegamyOrderService.java
+++ yn-core/src/main/java/com/yn/service/ApolegamyOrderService.java
@@ -13,60 +13,58 @@ import com.yn.utils.BeanCopy;
 
 @Service
 public class ApolegamyOrderService {
-	
-	@Autowired
-	 ApolegamyOrderDao apolegamyOrderdao;
-	
-	 public ApolegamyOrder findOne(Long id) {
-		 
-	        return apolegamyOrderdao.findOne(id);
-	    }
 
-	    public void save(ApolegamyOrder apolegamyOrder) {
-	        if(apolegamyOrder.getId()!=null){
-	        	ApolegamyOrder one = apolegamyOrderdao.findOne(apolegamyOrder.getId());
-	            try {
-	                BeanCopy.beanCopy(apolegamyOrder,one);
-	            } catch (Exception e) {
-	                e.printStackTrace();
-	            }
-	            apolegamyOrderdao.save(one);
-	        }else {
-	        	apolegamyOrderdao.save(apolegamyOrder);
-	        }
-	    }
-	
-	public void getapole(Order order,List<Long> list){
-		
-		ApolegamyOrder apolegamyOrder = new ApolegamyOrder();
-		String ids ="";
-		if(list.size()==0){
-			ids ="0";
+	@Autowired
+	ApolegamyOrderDao apolegamyOrderdao;
+
+	public ApolegamyOrder findOne(Long id) {
+
+		return apolegamyOrderdao.findOne(id);
+	}
+
+	public void save(ApolegamyOrder apolegamyOrder) {
+		if (apolegamyOrder.getId() != null) {
+			ApolegamyOrder one = apolegamyOrderdao.findOne(apolegamyOrder.getId());
+			try {
+				BeanCopy.beanCopy(apolegamyOrder, one);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+			apolegamyOrderdao.save(one);
+		} else {
+			apolegamyOrderdao.save(apolegamyOrder);
 		}
-		
+	}
+
+	public void getapole(Order order, List<Long> list) {
+
+		ApolegamyOrder apolegamyOrder = new ApolegamyOrder();
+		String ids = "";
+		if (list.size() == 0) {
+			ids = "0";
+		}
+
 		for (Long long1 : list) {
-			 ids +=long1.toString()+",";
+			ids += long1.toString() + ",";
 		}
 		apolegamyOrder.setApoIds(ids);
 		apolegamyOrder.setOrderId(order.getId());
 		apolegamyOrder.setPrice(order.getYnApolegamyPrice());
 		apolegamyOrder.setType(0);
-		
+
 		save(apolegamyOrder);
 	}
-	
-	
-	public List<Long> Transformation(String ids){
-		
+
+	public List<Long> Transformation(String ids) {
+
 		String[] apoId = ids.split(",");
-		
+
 		List<Long> list = new LinkedList<Long>();
-		
-		 for (int i = 0; i < apoId.length; i++) {
+
+		for (int i = 0; i < apoId.length; i++) {
 			list.add(Long.valueOf(apoId[i]));
 		}
-		
-		
+
 		return list;
 	}
 
